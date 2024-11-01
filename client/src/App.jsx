c5487f97003defd942d4579426455ed25137c309
Bunyawat Singkiporn <pupahero@gmail.com>
2024-11-01 12:42:57 +0700

    feat: create context for consumer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import React from "react"; //import React to use createContext
 
 import HomePage from "./pages/HomePage.jsx";
 import ViewProductPage from "./pages/ViewProductPage.jsx";
+
+
+export const UserDataContext = React.createContext(); // create context to use as provider
 
 function App() {
   const userData = {
@@ -13,12 +17,14 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/product/view/:id" element={<ViewProductPage />} />
-        </Routes>
-      </Router>
+      <UserDataContext.Provider value={{userData}}> {/*or user: userData and render {context.user.username} to AppHeader.jsx*/}
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/product/view/:id" element={<ViewProductPage />} />
+          </Routes>
+        </Router>
+      </UserDataContext.Provider>
     </div>
   );
 }
